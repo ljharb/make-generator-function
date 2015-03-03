f77c0cd2903a4967f10ee72cc0b845846d82f9c7
Jordan Harband <ljharb@gmail.com>
2015-03-03 10:30:54 -0800

    Reorganizing tests.

--- test/index.js
+++ test/index.js
@@ -3,13 +3,13 @@
 var test = require('tape');
 var genFunction = require('../');
 
-test('genFunction is undefined or a generator', function (t) {
-	if (genFunction) {
-		t.equal(typeof genFunction, 'function', 'genFunction is function');
-		t.equal(String(genFunction), 'function* () { var x = yield; return x || 42; }', 'genFunction has expected body');
-	} else {
-		t.equal(typeof genFunction, 'undefined', 'genFunction is undefined');
-	}
+test('generators supported', { skip: !genFunction }, function (t) {
+	t.equal(typeof genFunction, 'function', 'genFunction is function');
+	t.equal(String(genFunction), 'function* () { var x = yield; return x || 42; }', 'genFunction has expected body');
 	t.end();
 });
 
+test('generators not supported', { skip: genFunction }, function (t) {
+	t.equal(typeof genFunction, 'undefined', 'genFunction is undefined');
+	t.end();
+});
